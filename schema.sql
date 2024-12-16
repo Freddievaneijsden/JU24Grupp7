0bc8347f899df30c1161a9afc9f666486701474d
freddievaneijsden <freddievaneijsden@gmail.com>
2024-12-16 09:15:48 +0100

    Add data to database

--- schema.sql
+++ schema.sql
@@ -5,7 +5,8 @@ create table country
     country_id int not null auto_increment,
     country_name varchar(255) not null,
     country_language varchar(255) not null,
-    primary key (country_id)
+    primary key (country_id),
+    unique (country_name)
 );
 
 drop table country;
@@ -15,4 +16,21 @@ values ('Denmark', 'Danish'),
        ('Finland', 'Finnish'),
        ('Iceland', 'Icelandic'),
        ('Norway', 'Norwegian'),
-       ('Sweden', 'Swedish');
+       ('Sweden', 'Swedish'),
+       ('United States','English'),
+       ('Germany', 'German'),
+       ('Spain', 'Spanish'),
+       ('Argentina', 'Spanish'),
+       ('Chile', 'Spanish'),
+       ('Vietname', 'Vietnamese'),
+       ('Israel', 'Hebrew'),
+       ('El Salvador', 'Salvadorian'),
+       ('Brazil', 'Portuguese' ),
+       ('Egypt', 'Arabic'),
+       ('Korea', 'Korean');
+
+
+insert into country (country_name, country_language)
+values ('Denmark', 'Danish');
+
+
