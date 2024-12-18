f9a3e506145d3e06396e669c470066e305a15089
Biggienation <alexpet5888@gmail.com>
2024-12-18 13:02:06 +0100

    Animal

---
+++ jpa/src/main/java/quiz/AnimalQuiz.java
@@ -0,0 +1,40 @@
+package quiz;
+
+import java.util.Scanner;
+
+public class AnimalQuiz implements Quiz {
+    private final Scanner scanner = new Scanner(System.in);
+
+    @Override
+    public void play() {
+        questionOne();
+        questionTwo();
+        questionThree();
+        questionFour();
+        questionFive();
+    }
+    @Override
+    public void questionOne() {
+
+    }
+
+    @Override
+    public void questionTwo() {
+
+    }
+
+    @Override
+    public void questionThree() {
+
+    }
+
+    @Override
+    public void questionFour() {
+
+    }
+
+    @Override
+    public void questionFive() {
+
+    }
+}
