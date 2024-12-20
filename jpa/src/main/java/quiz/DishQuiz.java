134af5904763497aec35b79aa0de6010ab5330b7
Biggienation <alexpet5888@gmail.com>
2024-12-20 11:34:55 +0100

    added leader board

--- jpa/src/main/java/quiz/DishQuiz.java
+++ jpa/src/main/java/quiz/DishQuiz.java
@@ -5,29 +5,4 @@ public class DishQuiz implements Quiz {
     public void play() {
 
     }
-
-    @Override
-    public void questionOne() {
-
-    }
-
-    @Override
-    public void questionTwo() {
-
-    }
-
-    @Override
-    public void questionThree() {
-
-    }
-
-    @Override
-    public void questionFour() {
-
-    }
-
-    @Override
-    public void questionFive() {
-
-    }
 }
